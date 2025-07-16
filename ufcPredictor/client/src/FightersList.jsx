522d146118c9cc2b6a6b55b54eae054ee54a2ccb
Ethan <ethan.pascual@gmail.com>
2025-07-16 19:54:21 -0400

    search bar

--- ufcPredictor/client/src/FightersList.jsx
+++ ufcPredictor/client/src/FightersList.jsx
@@ -1,13 +1,31 @@
 import { currentFighters } from "./data/tempFighter"
 import FighterCard from "./components/FighterCard"
 import './index.css'
+import { useState } from "react"
 function FightersList(){
+    const [displayFighters, setFighters] = useState(currentFighters)
+    const [input, setInput] = useState('')
+    const handleSearch = (text) => {
+        setInput(text)
+        text = text.toLowerCase()
+        setFighters(currentFighters.filter(fighter => {
+            let lowerName = fighter.name.toLowerCase();
+            return lowerName.includes(text);
+        }));
+    }
+
     return (
         <>
             <h1>Fighters</h1>
+            <div className="input-wrapper">
+                <input 
+                    placeholder="Type to search..." 
+                    value={input} 
+                    onChange={e => handleSearch(e.target.value)}/>
+            </div>
             <div className="fighterGrid">
-                {currentFighters.map((currFighter, index) => (
-                    <FighterCard fighter={currFighter} />
+                {displayFighters.map((currFighter, index) => (
+                    <FighterCard key={index} fighter={currFighter} />
                 ))}
             </div>
             
--- ufcPredictor/client/src/components/FighterCard.jsx
+++ ufcPredictor/client/src/components/FighterCard.jsx
@@ -5,14 +5,16 @@ function FighterCard({fighter}){
     let link = "/fighter/" + fighter.name;
     return(
         <>
-        
-        <div className="card">
+        <div className='padded'>
         <Link to={link}>
+        <div className="card">
             <img src={fighter.image} />
             <h2>{fighter.name}</h2>
             <p>{fighter.wins}/{fighter.losses}/{fighter.draw}</p>
+        </div>
         </Link>
         </div>
+       
         
         </>
     )

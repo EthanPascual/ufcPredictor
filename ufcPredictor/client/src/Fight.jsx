72a2984e9190296f6690adb6acf69dfcec974f91
Ethan <ethan.pascual@gmail.com>
2025-07-17 23:22:05 -0400

    clickable table for fights

--- ufcPredictor/client/src/Fight.jsx
+++ ufcPredictor/client/src/Fight.jsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router";
+import {mockFights} from "./data/mockFight"
 
 function Fight(){
     let params = useParams();
+    console.log(mockFights)
 
     return(
         <>
             <h1>{params.fightId}</h1>
+            
         </>
     )
 }
--- ufcPredictor/client/src/FightsList.jsx
+++ ufcPredictor/client/src/FightsList.jsx
@@ -1,12 +1,39 @@
 import { mockFights } from "./data/mockFight"
-import {useTable} from "react-table"
+import { useNavigate } from "react-router"
 
 function FightsList(){
-    
+    const navigate = useNavigate()
     return (
         <>
             <h1>Fights:</h1>
-            
+            <div className="tableContainer">
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Fighter 1</th>
+                        <th>Fighhter 2</th>
+                        <th>Method</th>
+                        <th>Round</th>
+                        <th>Time</th>
+                        <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            mockFights.map((fight, index) => (
+                                <tr key={fight.fightId} onClick={() => navigate('/fight/' + fight.fightId)}>
+                                    <td>{fight.winner === fight.fighter1 ? <b>{fight.fighter1}</b> : fight.fighter1}</td>
+                                    <td>{fight.winner === fight.fighter2 ? <b>{fight.fighter2}</b> : fight.fighter2}</td>
+                                    <td>{fight.method}</td>
+                                    <td>{fight.round}</td>
+                                    <td>{fight.time}</td>
+                                    <td>{fight.date}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }

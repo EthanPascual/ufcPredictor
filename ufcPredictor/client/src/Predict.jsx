c9c4593548cbd9d261871574724731261cd83b7e
Ethan <ethan.pascual@gmail.com>
2025-07-24 03:43:31 -0400

    predict page now connected to backend

--- ufcPredictor/client/src/Predict.jsx
+++ ufcPredictor/client/src/Predict.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { currentFighters } from "./data/tempFighter"
+import axios from 'axios'
 
 function Predict(){
+    const [fighters, setFighters] = useState()
     const [fighter1, setFighter1] = useState('')
     const [fighter2, setFighter2] = useState('')
     const [result, setResult] = useState('')
+
+    useEffect(() => {
+        async function fetchData(){
+            await axios.get('http://localhost:3000/fighters').then(res => setFighters(res.data))
+        }
+        fetchData()
+    })
 
     const handleClick = () => {
         if(fighter1 && fighter2){
@@ -17,22 +26,29 @@ function Predict(){
 
     return (
         <>
-            <h1>Predict a Fight</h1>
+        {fighters ? (
+            <>
+                <h1>Predict a Fight</h1>
             <p>Select 2 Fighters:</p>
             <select value={fighter1} onChange={e=>(setFighter1(e.target.value))}>
                 <option value="" disabled>Fighter 1...</option>
-                {currentFighters.map((fighter, index) => (
+                {fighters.map((fighter, index) => (
                     <option key={index} value={fighter.name}>{fighter.name}</option>
                 ))}
             </select>
             <select value={fighter2} onChange={e=>(setFighter2(e.target.value))}>
                 <option value="" disabled>Fighter 2...</option>
-                {currentFighters.map((fighter, index) => (
+                {fighters.map((fighter, index) => (
                     <option key={index} value={fighter.name}>{fighter.name}</option>
                 ))}
             </select>
             <button onClick={handleClick}>Predict</button>
             {result && <p>{result}</p>}
+            </>
+        ) : (
+            <p>Loading....</p>
+        )}
+            
         </>
     )
 }

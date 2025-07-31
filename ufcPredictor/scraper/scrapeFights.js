9d9738beb747863af6d955a47b8133f461f245b4
Ethan <ethan.pascual@gmail.com>
2025-07-30 21:24:38 -0400

    scraper for all fights within each event

--- ufcPredictor/scraper/scrapeFights.js
+++ ufcPredictor/scraper/scrapeFights.js
@@ -24,7 +24,36 @@ try{
 }
 
 console.log("Event Urls done scraping")
-eventUrls.shift() //removing first event, because that is there "up next" event, and hasn't happened yet
+eventUrls.shift() //removing first event, because that is their "up next" event, and hasn't happened yet
 console.log("There are " + eventUrls.length + " events")
 console.log(eventUrls[0])
 
+let fightUrls = []
+let counter = 0
+for(const url of eventUrls){
+    counter += 1;
+    console.log("Scraping URL: " + counter)
+    try {
+        const response = await axios(url)
+        const $ = cheerio.load(response.data)
+        $(".b-fight-details__table-body tr").each((index, element) => {
+            const href = $(element).attr('data-link')
+            if(href){
+                fightUrls.push(href)
+            }
+        })
+    } catch (error) {
+        console.error(`Scraping Failed for ${url}`, error.message)
+    }
+
+    await delay(1000)
+}
+
+console.log("Done Scraping Fight Urls")
+console.log("There are " + fightUrls.length + " fights")
+console.log(fightUrls[0])
+
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
